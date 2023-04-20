3df2fb904539fd0d71b60d3d0c894e63206b36b9
Carlos Salgado <chpsalgado@hotmail.com>
2023-04-20 01:15:24 -0300

    fg

--- Aulas-html5/javascript/concatenacao.js
+++ Aulas-html5/javascript/concatenacao.js
@@ -1,5 +1,5 @@
-var nome = prompt('Digite o seu nome:')
-var idade = prompt('Digite a sua idade:')
+var nome = prompt('Digite o seu nome:');
+var idade = prompt('Digite a sua idade:');
 
-document.write('<h1>Olá '  + nome + ', você tem ' + idade +' anos?<h1>')
+document.write('<h1>Olá '  + nome + ', você tem ' + idade +' anos?<h1>');
 
