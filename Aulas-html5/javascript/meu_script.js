2800ab6e96f294714b7781cc7d8d7fbf5f3813fc
Carlos Salgado <chpsalgado@hotmail.com>
2023-04-20 01:02:39 -0300

    i

--- Aulas-html5/javascript/meu_script.js
+++ Aulas-html5/javascript/meu_script.js
@@ -1,2 +1,16 @@
+/*
 alert('Olá, seja bem vindo ao curso')
-document.getElementById('nome').value = 'Oi'
+document.getElementById('nome').value = 'Oi'*/
+
+//string
+var texto = 'Curso de JavaScript' 
+
+//number
+var numeroInteiro = -7
+var numeroFracionado = 11210.75
+
+//boolean
+var teste = true
+
+//document.write(texto)
+console.log(texto)
