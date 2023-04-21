aa6a19fa7ace4bb998b1be62d373a075c6a2db74
Carlos Salgado <chpsalgado@hotmail.com>
2023-04-20 23:12:27 -0300

    Corrigido

--- Aulas-html5/javascript/If-else/if-else.js
+++ Aulas-html5/javascript/If-else/if-else.js
@@ -5,7 +5,14 @@ var nota2 = prompt('Dgite a 2ª nota:');
 var nota3 = prompt('Dgite a 3ª nota:');
 var nota4 = prompt('Dgite a 4ª nota:');
 
-var notaGeral = nota1 + nota2 + nota3 + nota4;
+
+nota1 = parseInt(nota1)
+nota2 = parseInt(nota2)
+nota3 = parseInt(nota3)
+nota4 = parseInt(nota4)
+
+
+var notaGeral = ( nota1 + nota2 + nota3 + nota4 ) / 4;
 
 var media = 7
 
