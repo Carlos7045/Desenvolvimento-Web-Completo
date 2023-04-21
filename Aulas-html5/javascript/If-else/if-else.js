56655f8536a8c9436e316e772ff1ec45a3c04a54
Carlos Salgado <chpsalgado@hotmail.com>
2023-04-20 22:32:35 -0300

    j

--- Aulas-html5/javascript/If-else/if-else.js
+++ Aulas-html5/javascript/If-else/if-else.js
@@ -1,5 +1,19 @@
+var nome = prompt('Nome do aluno:');
+var idade = prompt('Idade do Aluno:');
+var nota1 = prompt('Dgite a 1ª nota:');
+var nota2 = prompt('Dgite a 2ª nota:');
+var nota3 = prompt('Dgite a 3ª nota:');
+var nota4 = prompt('Dgite a 4ª nota:');
+
+var notaGeral = nota1 + nota2 + nota3 + nota4;
+
+var media = 7
 
 
-if(true){
-  document.write('Entrou dentro do bloco if (verdadeiro)')
+
+if(notaGeral >= media){
+  document.write('O ', nome,  ', Passou de ano, com a nota:', notaGeral );
+}
+else {
+  document.write('O aluno', nome, 'Não passou de ano, sua nota foi;', notaGeral);
 }
